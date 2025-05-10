0750509ecea017c3a92eb64e5b7bb44125d033be
amaydixit11 <amaydixit11@gmail.com>
2025-05-10 10:56:53 +0530

    scheme manager initialization

--- bpp/backend/pkg/types/payload/job-application/job-application.go
+++
@@ -1,6 +0,0 @@
-package jobapplication
-
-type UpdateJobApplicationStatusRequest struct {
-	// @Enum(APPLICATION_ACCEPTED, APPLICATION_REJECTED, ASSESSMENT_IN_PROGRESS, OFFER_REJECTED, OFFER_ACCEPTED, OFFER_EXTENDED, CANCELLED)
-	Status string `json:"status"`
-}
